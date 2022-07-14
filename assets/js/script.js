a6dd0d0b89488135543b31141346f8428e6717c9
Maria Clara da Silva Rodrigues <mariarodrigues.dev@gmail.com>
2022-07-14 09:56:38 -0300

    Colocando responsividade e animação

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '50px',
+    duration: 2000,
+    reset: true
+});
+
+ScrollReveal().reveal('.destaque', { delay: 200 });
+ScrollReveal().reveal('.descricaoDestaque', { delay: 400 });
+ScrollReveal().reveal('.quadrado-cinza', { delay: 500 });
+ScrollReveal().reveal('.jordan', { delay: 600 });
